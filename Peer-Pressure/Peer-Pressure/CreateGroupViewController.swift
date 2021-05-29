1af12d859788872e9e84d00b8ededccf9990b977
Leo <lvaldiiva.jr@gmail.com>
2021-05-29 01:19:02 -0700

    add group picture selection

--- Peer-Pressure/Peer-Pressure/CreateGroupViewController.swift
+++ Peer-Pressure/Peer-Pressure/CreateGroupViewController.swift
@@ -6,13 +6,39 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class CreateGroupViewController: UIViewController {
+class CreateGroupViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var groupPicture: UIImageView!
     @IBAction func onBackButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af.imageScaled(to: size)
+        
+        groupPicture.image = scaledImage
+        dismiss(animated: true, completion: nil)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
