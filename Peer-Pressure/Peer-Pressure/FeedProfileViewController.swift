bfb0901aa4b912f7e31aeca22687e7667283357b
Adrian Lindell <adrianlindell412@gmail.com>
2021-05-29 14:14:19 -0400

    added feed profile view

---
+++ Peer-Pressure/Peer-Pressure/FeedProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedProfileViewController.swift
+//  Peer-Pressure
+//
+//  Created by Adrian Lindell on 5/29/21.
+//
+
+import UIKit
+import Parse
+
+class FeedProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
